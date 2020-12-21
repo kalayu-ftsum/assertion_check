a486265a7628621075bd9c0e8fb2c2627e5b0d99
kalayu-ftsum <kalayuftsumm@gmail.com>
2020-12-21 22:25:07 +0300

    final assertion code

--- assert.py
+++ assert.py
@@ -5,10 +5,13 @@ driver.get('https://oto.repair/')
 myMenuText = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/div[2]/nav/ul/li[2]/a')
 textTobeAsserted="Danh sách tiệm"
 try:
-    assert textTobeAsserted in myMenuText.text
+    assert textTobeAsserted , myMenuText.text
     print("{} exists on the main menu".format(textTobeAsserted))
 except AssertionError:
-    print("{} doesn't exist".format(textTobeAsserted))
+    if len(myMenuText.text) >0:
+        print("the main menu doesn't have any text")
+    else:
+         print("{} doesn't exist".format(textTobeAsserted))
 
 driver.close()
     
